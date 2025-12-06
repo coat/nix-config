7037a1d3e9422afeb0e598f7cb128ec653dc30cf
Kent Smith <kentsmith@gmail.com>
2025-12-05 17:26:56 -0800

    use new events attrmap format

--- users/features/desktop/sway.nix
+++ users/features/desktop/sway.nix
@@ -111,19 +111,14 @@
     swayidle = {
       enable = true;
 
-      events = [
-        # { event = "timeout 300"; command = "${pkgs.swaylock}/bin/swaylock -fF -c 000000"; }
-        # { event = "timeout 600"; command = "swaymsg \"output * dpms off\""; }
-        {
-          event = "after-resume";
-          command = "swaymsg \"output * dpms on\"";
-        }
-        {
-          event = "before-sleep";
-          command = "${pkgs.swaylock}/bin/swaylock -fF -c 000000";
-        }
-      ];
+      events = {
+        "after-resume" = "swaymsg \"output * dpms on\"";
+        "before-sleep" = "${pkgs.swaylock}/bin/swaylock -fF -c 000000";
+      };
+      # { event = "timeout 300"; command = "${pkgs.swaylock}/bin/swaylock -fF -c 000000"; }
+      # { event = "timeout 600"; command = "swaymsg \"output * dpms off\""; }
     };
+
     wob.enable = true;
   };
 }
