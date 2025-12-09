215c0ba003b1a7605f9c0c636476421ea94c16fb
Kent Smith <kentsmith@gmail.com>
2025-12-08 19:06:26 -0800

    fix wireguard question mark question mark

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -4,15 +4,15 @@
   networking.wg-quick.interfaces = {
     wg0 = {
       autostart = false;
-      address = ["10.20.155.53/32"];
+      address = ["10.17.205.221/32"];
       dns = ["10.0.0.243"];
       privateKeyFile = config.clan.core.vars.generators.wireguard-vpn.files.wg-key.path;
 
       peers = [
         {
-          publicKey = "5u3eMHBFKLCzKcezy/Xd/F7EqNP75Ixw0ud9hlKYkjg=";
+          publicKey = "WbYarp116unGvL3OjsplJSj686LuY2dVIuius3lDAGM=";
           allowedIPs = ["0.0.0.0/0"];
-          endpoint = "149.22.95.149:1337";
+          endpoint = "212.56.52.56:1337";
           persistentKeepalive = 25;
         }
       ];
