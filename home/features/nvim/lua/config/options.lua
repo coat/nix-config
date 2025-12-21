f3182a204b22c29055612b4cd282736fd9bd6175
Kent Smith <kentsmith@gmail.com>
2025-12-20 18:21:25 -0800

    cleanup nvim config instead of going to work

--- home/features/nvim/lua/config/options.lua
+++ home/features/nvim/lua/config/options.lua
@@ -3,11 +3,3 @@
 -- Add any additional options here
 vim.g.snacks_animate = false
 vim.opt.wrap = true
-vim.opt.clipboard = "unnamed"
---
--- Use internal formatting for bindings like gq.
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    vim.bo[args.buf].formatexpr = nil
-  end,
-})
