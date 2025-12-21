751ce74fb4ad58608819397590a441a2b3ba0a5b
Kent Smith <kentsmith@gmail.com>
2025-12-20 18:21:24 -0800

    add docker module

--- hosts/work/default.nix
+++ hosts/work/default.nix
@@ -8,6 +8,7 @@
 
     ../common/global
     ../common/optional/wireless.nix
+    ../common/optional/docker.nix
     ../common/optional/xorg.nix
   ];
 
@@ -34,29 +35,6 @@
   };
 
   virtualisation = {
-    docker = {
-      enable = true;
-
-      daemon.settings = {
-        # userland-proxy = false;
-        # experimental = true;
-        # metrics-addr = "0.0.0.0:9323";
-        # ipv6 = true;
-        # fixed-cidr-v6 = "fd00::/80";
-
-        log-opts = {
-          compress = "false";
-        };
-      };
-
-      logDriver = "local";
-      # rootless = {
-      #   enable = true;
-      #   setSocketVariable = true;
-      # };
-      storageDriver = "btrfs";
-    };
-
     libvirtd = {
       enable = true;
       qemu = {
