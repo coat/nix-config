abffc041f4af49bf1444198cfb398402db55d2e5
Kent Smith <kentsmith@gmail.com>
2025-12-20 18:21:24 -0800

    add awsvpnclient

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,9 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
-{pkgs ? import <nixpkgs> {}, ...}: rec {
+{pkgs ? import <nixpkgs> {}, ...}: {
   andros-cli = pkgs.callPackage ./andros-cli {};
   ez80asm = pkgs.callPackage ./ez80asm {};
   fab-agon-emulator = pkgs.callPackage ./fab-agon-emulator {};
   pop = pkgs.callPackage ./pop {};
+  awsvpnclient = pkgs.callPackage ./awsvpnclient {};
 }
