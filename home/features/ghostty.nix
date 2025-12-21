d7b91844ce9f14bcc77dfde6f8c1628d23db2a33
Kent Smith <kentsmith@gmail.com>
2025-12-20 18:21:24 -0800

    add install.sh for devcontainer dotfiles

---
+++ home/features/ghostty.nix
@@ -0,0 +1,14 @@
+{
+  programs.ghostty = {
+    enable = true;
+    enableZshIntegration = true;
+    installVimSyntax = true;
+    settings = {
+      font-family = "Iosevka";
+      font-size = 12;
+      window-decoration = false;
+      resize-overlay = "never";
+      theme = "starlight";
+    };
+  };
+}
