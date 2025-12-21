f8808c6d9f9f79524a869a6a7563b1224e53914c
Kent Smith <kentsmith@gmail.com>
2025-12-20 18:21:24 -0800

    this makes gw do what gq used to do... somehow

--- home/sadbeast/features/nvim/lua/config/options.lua
+++ home/sadbeast/features/nvim/lua/config/options.lua
@@ -4,3 +4,10 @@
 vim.g.snacks_animate = false
 vim.opt.wrap = true
 vim.opt.clipboard = "unnamed"
+--
+-- Use internal formatting for bindings like gq.
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    vim.bo[args.buf].formatexpr = nil
+  end,
+})
