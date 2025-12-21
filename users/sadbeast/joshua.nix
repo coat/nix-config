2e90dbd039b7ec3044f3c5c40fe1efafbef1eb84
Kent Smith <kentsmith@gmail.com>
2025-12-20 18:21:26 -0800

    totally forgot my monitor is a touchscreen

--- users/sadbeast/joshua.nix
+++ users/sadbeast/joshua.nix
@@ -1,14 +1,22 @@
 {
   imports = [./desktop-base.nix];
 
-  wayland.windowManager.sway.config.output = {
-    HDMI-A-1 = {
-      resolution = "1920x1080";
-      position = "0,0";
+  wayland.windowManager.sway.config = {
+    output = {
+      HDMI-A-1 = {
+        resolution = "1920x1080";
+        position = "0,0";
+      };
+      DVI-D-1 = {
+        resolution = "1920x1080";
+        position = "1920,0";
+      };
     };
-    DVI-D-1 = {
-      resolution = "1920x1080";
-      position = "1920,0";
+
+    input = {
+      "8746:43:ILITEK_ILITEK_Multi-Touch" = {
+        map_to_output = "HDMI-A-1";
+      };
     };
   };
 }
