6c7fea728a1dae5ad3f1c20c7f5025bd7e4b7577
Kent Smith <kentsmith@gmail.com>
2025-12-20 18:21:23 -0800

    add my fav colorscheme

---
+++ home/sadbeast/features/nvim/lua/plugins/core.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "tinted-theming/tinted-vim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme base16-default-dark]])
+    end,
+  },
+}
