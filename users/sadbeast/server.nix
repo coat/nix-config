ba1289944fefb62173cfd2bbcd44bb641d583a6d
Kent Smith <kentsmith@gmail.com>
2025-12-20 18:21:26 -0800

    opt in to stylix modules on servers

--- users/sadbeast/server.nix
+++ users/sadbeast/server.nix
@@ -17,6 +17,18 @@
       inputs.nixvim.homeModules.nixvim
       inputs.nix-index-database.homeModules.nix-index
       inputs.stylix.homeModules.stylix
+      ../../modules/stylix.nix
+      {
+        stylix = {
+          autoEnable = false;
+
+          targets.btop.enable = true;
+          targets.fzf.enable = true;
+          targets.nixvim.enable = true;
+          targets.starship.enable = true;
+          targets.tmux.enable = true;
+        };
+      }
     ];
 
     users.sadbeast.imports = [./home.nix];
