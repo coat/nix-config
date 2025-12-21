826fb690279de8b72bd69037c0f686fb431b27ac
Kent Smith <kentsmith@gmail.com>
2025-12-20 18:21:25 -0800

    add aws profile zsh functions

--- home/kent/darwin.nix
+++ home/kent/darwin.nix
@@ -1,8 +1,9 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ../features/global.nix
     ../features/qutebrowser.nix
     ../features/devcontainers.nix
+    ../features/nvim.nix
     # ../features/ghostty.nix
   ];
 
@@ -12,8 +13,17 @@
     stateVersion = "25.05";
 
     sessionVariables = {
+      AWS_PROFILE = "andros-dev-1.SoftwareEngineer";
       CODEARTIFACT_AUTH_CMD = "aws codeartifact get-authorization-token --domain andros --domain-owner 111491220182 --region us-east-2 --query authorizationToken --output text";
     };
+
+    packages = with pkgs; [
+      awscli2
+      docker-buildx
+      httpie
+      nodejs
+      ruby-lsp
+    ];
   };
 
   programs.git = {
