527fb1d2a9977187a77d154ffa30df56e032f335
Kent Smith <kentsmith@gmail.com>
2025-12-20 18:21:25 -0800

    cleanup unused overlay stuff

--- home/features/global.nix
+++ home/features/global.nix
@@ -1,6 +1,4 @@
 {
-  inputs,
-  outputs,
   pkgs,
   lib,
   ...
@@ -27,23 +25,6 @@
   };
 
   nixpkgs = {
-    overlays = [
-      # Add overlays your own flake exports (from overlays and pkgs dir):
-      outputs.overlays.additions
-      # outputs.overlays.modifications
-      # outputs.overlays.stable-packages
-
-      # You can also add overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-
     config = {
       allowUnfree = true;
       allowUnfreePredicate = pkg:
