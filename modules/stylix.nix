c506dae1a7438756525c5bd88aa229f28f97dbf9
Kent Smith <kentsmith@gmail.com>
2026-01-31 16:21:02 -0800

    try a new theme

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -2,7 +2,7 @@
   stylix = {
     enable = true;
 
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/default-dark.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/eighties.yaml";
     polarity = "dark";
 
     fonts = with pkgs; {
