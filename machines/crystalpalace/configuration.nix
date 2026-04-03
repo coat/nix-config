3792b51aa53e73c11fd81ed75b17bdaec9ad9833
Kent Smith <kentsmith@gmail.com>
2026-04-03 14:49:36 -0700

    accelerate video?

--- machines/crystalpalace/configuration.nix
+++ machines/crystalpalace/configuration.nix
@@ -1,4 +1,8 @@
-{inputs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   imports = [
     inputs.nixarr.nixosModules.default
     ../../modules/global.nix
@@ -21,4 +25,13 @@
     device = "/dev/disk/by-uuid/98be24fc-82f9-455f-a7c2-afbea9ff67fb";
     fsType = "ext4";
   };
+
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # For Broadwell (2014) or newer processors. LIBVA_DRIVER_NAME=iHD
+    ];
+  };
+
+  environment.sessionVariables = {LIBVA_DRIVER_NAME = "iHD";};
 }
--- modules/immich.nix
+++ modules/immich.nix
@@ -1,9 +1,12 @@
 {...}: {
   services.immich = {
     enable = true;
+    accelerationDevices = null;
     port = 2283;
     host = "0.0.0.0";
     openFirewall = true;
     mediaLocation = "/mnt/files/immich";
   };
+
+  users.users.immich.extraGroups = ["video" "render"];
 }
