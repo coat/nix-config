f511a4a0644cb40222675b5944edd68e8c987a52
Kent Smith <kentsmith@gmail.com>
2026-04-04 10:14:52 -0700

    move qutebrowser specific xdg settings to qutebrowser module

--- users/features/desktop/default.nix
+++ users/features/desktop/default.nix
@@ -50,14 +50,7 @@
   gtk.enable = true;
 
   xdg = {
-    mimeApps = {
-      enable = true;
-      defaultApplications = {
-        "text/html" = "org.qutebrowser.qutebrowser.desktop";
-        "x-scheme-handler/http" = "org.qutebrowser.qutebrowser.desktop";
-        "x-scheme-handler/https" = "org.qutebrowser.qutebrowser.desktop";
-      };
-    };
+    mimeApps.enable = true;
 
     userDirs = let
       homeDir = config.home.homeDirectory;
--- users/features/desktop/qutebrowser.nix
+++ users/features/desktop/qutebrowser.nix
@@ -1,5 +1,11 @@
-{...}: {
+{
   programs.qutebrowser = {
     enable = true;
+  };
+
+  xdg.mimeApps.defaultApplications = {
+    "text/html" = "org.qutebrowser.qutebrowser.desktop";
+    "x-scheme-handler/http" = "org.qutebrowser.qutebrowser.desktop";
+    "x-scheme-handler/https" = "org.qutebrowser.qutebrowser.desktop";
   };
 }
