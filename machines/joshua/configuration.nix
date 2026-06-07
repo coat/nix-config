a9dca31d0e02882685c8e50a4bafbc5ff5f9384e
Kent Smith <kentsmith@gmail.com>
2026-06-07 12:12:26 -0700

    remove gtk4 theme default

--- machines/joshua/configuration.nix
+++ machines/joshua/configuration.nix
@@ -5,6 +5,10 @@
 
   networking.hostName = "joshua";
 
+  # Pin mDNS to the LAN NIC so avahi doesn't announce on docker/veth/zerotier
+  # interfaces and rename itself away from joshua.local via false collisions.
+  services.avahi.interfaces = ["enp0s31f6"];
+
   system.stateVersion = "24.11";
 
   users.groups.libvirtd.members = ["sadbeast"];
--- machines/wopr/configuration.nix
+++ machines/wopr/configuration.nix
@@ -5,6 +5,10 @@
 
   networking.hostName = "wopr";
 
+  # Pin mDNS to the LAN NIC so avahi doesn't announce on docker/veth/zerotier
+  # interfaces and rename itself away from wopr.local via false collisions.
+  services.avahi.interfaces = ["wlp3s0"];
+
   networking.firewall.allowedTCPPorts = [3000];
 
   services.logind.settings.Login.HandleLidSwitchExternalPower = "ignore";
